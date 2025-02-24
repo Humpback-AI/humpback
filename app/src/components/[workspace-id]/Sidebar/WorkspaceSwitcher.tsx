0d178ae1c921ed7ea01bf67959eed4bc425bce11
lws803 <lws803@gmail.com>
2025-02-24 18:55:30 -0500

    refactored code

--- app/src/components/[workspace-id]/Sidebar/WorkspaceSwitcher.tsx
+++ app/src/components/[workspace-id]/Sidebar/WorkspaceSwitcher.tsx
@@ -12,27 +12,7 @@ import {
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 
-// Add color generation utility
-function getColorFromName(name: string): string {
-  const colors = [
-    "bg-purple-600",
-    "bg-blue-600",
-    "bg-indigo-600",
-    "bg-pink-600",
-    "bg-teal-600",
-    "bg-green-600",
-    "bg-red-600",
-  ];
-
-  // Create a simple hash from the name
-  const hash = name.split("").reduce((acc, char) => {
-    return char.charCodeAt(0) + ((acc << 5) - acc);
-  }, 0);
-
-  // Use the hash to select a color
-  const index = Math.abs(hash) % colors.length;
-  return colors[index];
-}
+import { getColorFromName } from "./WorkspaceSwitcher/lib";
 
 interface Workspace {
   id: string;
