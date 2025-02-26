91a78ed5dc468895672fed6b59ae5a500ae80a76
lws803 <lws803@gmail.com>
2025-02-26 08:26:42 -0500

    fixed pipes

--- app/src/contexts/TinybirdContext.tsx
+++ app/src/contexts/TinybirdContext.tsx
@@ -7,6 +7,7 @@ import {
   useCallback,
   ReactNode,
 } from "react";
+
 import { generateJWT } from "@/lib/tinybird/token";
 
 interface TinybirdContextType {
--- app/src/lib/tinybird/token.ts
+++ app/src/lib/tinybird/token.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 const TINYBIRD_SIGNING_TOKEN = process.env.TINYBIRD_SIGNING_TOKEN ?? "";
 const WORKSPACE_ID = process.env.TINYBIRD_WORKSPACE ?? "";
-const PIPE_ID = "top_airlines";
+const PIPE_ID = "t_fb9b9a15a78c4a2d8f8ffa18a8c2552b";
 
 export async function generateJWT() {
   const next10minutes = new Date();
@@ -12,7 +12,7 @@ export async function generateJWT() {
 
   const payload = {
     workspace_id: WORKSPACE_ID,
-    name: "my_demo_jwt",
+    name: "app_jwt",
     exp: Math.floor(next10minutes.getTime() / 1000),
     scopes: [
       {
